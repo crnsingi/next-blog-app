d52350c1f93686832680204adcaa973eda02829e
Cesar <cesarnsingi@hotmail.com>
2024-10-19 20:47:49 +0400

    add priority to h1

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get started <Image src={assets.arrow}/> </button>
         </div>
         <div className='text-center my-8'>
-            <h1>Latest Blogs</h1>
+            <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
 
         </div>
     </div>
