9389797e3db1f0393b394332b5fd0908a2e21eed
Cesar <cesarnsingi@hotmail.com>
2024-10-19 19:17:43 +0400

    add property to image

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
         <div className='flex justify-between items-center'>
-            <Image src={assets.logo} />
+            <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
 
         </div>
     </div>
