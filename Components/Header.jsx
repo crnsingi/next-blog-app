25283d3025312248c2a010a3365fd565febc573b
Cesar <cesarnsingi@hotmail.com>
2024-10-19 20:41:01 +0400

    add h1 title

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -8,6 +8,9 @@ const Header = () => {
         <div className='flex justify-between items-center'>
             <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
             <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get started <Image src={assets.arrow}/> </button>
+        </div>
+        <div className='text-center my-8'>
+            <h1>Latest Blogs</h1>
 
         </div>
     </div>
