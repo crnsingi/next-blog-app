3bb22b500a962fe807385d0c5389a3c008c15411
Cesar <cesarnsingi@hotmail.com>
2024-10-20 11:09:08 +0400

    add h5 title

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -7,6 +7,10 @@ const BlogItem = () => {
     <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
     <Image src={blog_data[0].image} alt='' width={400} height={400} className='border-b border-black' />
     <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{blog_data[0].category}</p>
+    <div className='p-5'>
+        <h5>{blog_data[0].title}</h5>
+
+    </div>
     </div>
   )
 }
