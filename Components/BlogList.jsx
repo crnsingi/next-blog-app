2060e4a933118864972ee05cbec27ebb06eccd19
Cesar <cesarnsingi@hotmail.com>
2024-10-20 22:46:38 +0400

    add design tp Blog List

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const BlogList = () => {
   return (
-    <div>BlogList</div>
+    <div>
+        <div className='flex justify-center gap-6 my-10'>
+            <button className='bg-black text-white py-1 px-4 rounded-sm'>All</button>
+            <button></button>
+            <button>Technology</button>
+            <button>Startup</button>
+            <button>Lifestyle</button>
+
+        </div>
+
+    </div>
   )
 }
 
