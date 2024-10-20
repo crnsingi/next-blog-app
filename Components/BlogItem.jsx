7477dbd4115d6a41d29e761397f448bfcfb9308f
Cesar <cesarnsingi@hotmail.com>
2024-10-20 11:03:11 +0400

    add a paragraph

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -6,7 +6,7 @@ const BlogItem = () => {
   return (
     <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
     <Image src={blog_data[0].image} alt='' width={400} height={400} className='border-b border-black' />
-
+    <p>{blog_data[0].category}</p>
     </div>
   )
 }
