108e65cc0becd7e604d453478a4a27370ec4a3cb
Cesar <cesarnsingi@hotmail.com>
2024-10-21 08:11:47 +0400

    add social logo

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -7,8 +7,9 @@ const Footer = () => {
     <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
         <Image src={assets.logo_light} alt='' width={120} />
         <p className='text-sm text-white'>All right reserved. Copyright @blogger</p>
-
-
+        <div className='flex'>
+            <Image src={assets.facebook_icon} alt='' width={40} />
+        </div>
     </div>
   )
 }
