8cda7786ce4ebfdf8c5140dc8331b5a8a9ef8ee8
Cesar <cesarnsingi@hotmail.com>
2024-10-21 10:35:37 +0400

    check route

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -13,9 +13,9 @@ const BlogItem = ({title,description,category,image,id}) => {
     <div className='p-5'>
         <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{title}</h5>
         <p className='mb-3 text-sm tracking-tight text-gray-700'>{description}</p>
-        <div className='inline-flex items-center py-2 font-semibold text-center'>
+        <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center'>
             Read more <Image src={assets.arrow} className='ml-2' alt='' width={12}/>
-        </div>
+        </Link>
     </div>
     </div>
   )
