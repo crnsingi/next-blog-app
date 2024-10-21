9d010548d0f5f647f5586bc8201b53958d77680d
Cesar <cesarnsingi@hotmail.com>
2024-10-21 07:38:01 +0400

    create footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
 
     </div>
   )
