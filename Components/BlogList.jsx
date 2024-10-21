74f801301592cc212bbb7fb2507eb5cc6276756f
Cesar <cesarnsingi@hotmail.com>
2024-10-21 05:24:35 +0400

    set menu

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -9,10 +9,10 @@ const BlogList = () => {
   return (
     <div>
         <div className='flex justify-center gap-6 my-10'>
-            <button onClick={} className='bg-black text-white py-1 px-4 rounded-sm'>All</button>
-            <button onClick={}>Technology</button>
-            <button onClick={}>Startup</button>
-            <button onClick={}>Lifestyle</button>
+            <button onClick={()=>setMenu('All')} className='bg-black text-white py-1 px-4 rounded-sm'>All</button>
+            <button onClick={()=>setMenu('Technology')}>Technology</button>
+            <button onClick={()=>setMenu('Startup')}>Startup</button>
+            <button onClick={()=>setMenu('Lifestyle')}>Lifestyle</button>
         </div>
         <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:max-24 '>
             {blog_data.map((item,index)=>{
