2d8e47c4b7862a43ff91a888d0219b568f1d1c57
Cesar <cesarnsingi@hotmail.com>
2024-10-21 05:28:26 +0400

    update with className

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -9,10 +9,10 @@ const BlogList = () => {
   return (
     <div>
         <div className='flex justify-center gap-6 my-10'>
-            <button onClick={()=>setMenu('All')} className='bg-black text-white py-1 px-4 rounded-sm'>All</button>
-            <button onClick={()=>setMenu('Technology')}>Technology</button>
-            <button onClick={()=>setMenu('Startup')}>Startup</button>
-            <button onClick={()=>setMenu('Lifestyle')}>Lifestyle</button>
+            <button onClick={()=>setMenu('All')} className={menu==="All"?'bg-black text-white py-1 px-4 rounded-sm' :""}>All</button>
+            <button onClick={()=>setMenu('Technology')} className={menu==="All"?'bg-black text-white py-1 px-4 rounded-sm' :""}>Technology</button>
+            <button onClick={()=>setMenu('Startup')} className={menu==="All"?'bg-black text-white py-1 px-4 rounded-sm' :""}>Startup</button>
+            <button onClick={()=>setMenu('Lifestyle')} className={menu==="All"?'bg-black text-white py-1 px-4 rounded-sm' :""}>Lifestyle</button>
         </div>
         <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:max-24 '>
             {blog_data.map((item,index)=>{
