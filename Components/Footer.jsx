7dcfc1fb41c57d5a1ef41e0134f06d4f4bfd14f5
Cesar <cesarnsingi@hotmail.com>
2024-10-21 08:13:09 +0400

    social logos

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -9,6 +9,8 @@ const Footer = () => {
         <p className='text-sm text-white'>All right reserved. Copyright @blogger</p>
         <div className='flex'>
             <Image src={assets.facebook_icon} alt='' width={40} />
+            <Image src={assets.twitter_icon} alt='' width={40} />
+            <Image src={assets.googleplus_icon} alt='' width={40} />
         </div>
     </div>
   )
