463426ac07920767353be3b815b11a85df34dc0c
Cesar <cesarnsingi@hotmail.com>
2024-10-21 04:48:15 +0400

    add on click property

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -9,11 +9,10 @@ const BlogList = () => {
   return (
     <div>
         <div className='flex justify-center gap-6 my-10'>
-            <button className='bg-black text-white py-1 px-4 rounded-sm'>All</button>
-            <button></button>
-            <button>Technology</button>
-            <button>Startup</button>
-            <button>Lifestyle</button>
+            <button onClick={} className='bg-black text-white py-1 px-4 rounded-sm'>All</button>
+            <button onClick={}>Technology</button>
+            <button onClick={}>Startup</button>
+            <button onClick={}>Lifestyle</button>
         </div>
         <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:max-24 '>
             {blog_data.map((item,index)=>{
