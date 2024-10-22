3437a1b1bce7fdd4bedbe29fa4f5f2522fe8c320
Cesar <cesarnsingi@hotmail.com>
2024-10-22 12:16:40 +0400

    blog post page

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -1,6 +1,8 @@
 'use client'
 import { assets, blog_data } from '@/Assets/assets';
+import Footer from '@/Components/Footer';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
 const page = ({params}) => {
@@ -23,7 +25,9 @@ const page = ({params}) => {
 return (data?<>
     <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
         <div className='flex justify-between items-center'>
+            <Link href='/'>
             <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
+            </Link>
             <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
                 Get started <Image src={assets.arrow} alt=''/>
                 </button>
@@ -47,8 +51,18 @@ return (data?<>
         <h3 className='my-5 text-[18px] font-semibold'>Step 3: Self-Reflection and Goal Setting</h3>
         <p className='my-3'>Beofre you can manage your lifestyle</p>
         <p className='my-3'>Beofre you can manage your lifestyle</p>
-
+        <h3 className='my-5 text-[18px] font-semibold'>Conclusion</h3>
+        <p className='my-3'>Managing your lifestyle</p>
+        <div className='my-24'>
+            <p className='text-black font font-semibold my-4'>Share this articule on social media</p>
+            <div className='flex'>
+                <Image src={assets.facebook_icon} width={50} alt='' />
+                <Image src={assets.twitter_icon} width={50} alt='' />
+                <Image src={assets.googleplus_icon} width={50} alt='' />
+            </div>
+        </div>
     </div>
+    <Footer />
     </>:<></>
   )
 }
