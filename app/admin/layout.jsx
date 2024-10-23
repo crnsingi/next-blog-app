a99802113547ae4561df22d054960de066819d32
Cesar <cesarnsingi@hotmail.com>
2024-10-23 10:41:47 +0400

    add property to admin

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,6 +1,11 @@
+import Sidebar from "@/Components/AdminComponents/Sidebar";
+
 export default function Layout({children}){
     return (
         <>
+           <div className="flex">
+            <Sidebar/>
+           </div>
         {children}
         </>
     )
