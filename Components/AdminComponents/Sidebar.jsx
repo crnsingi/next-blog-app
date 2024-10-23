28576bc7001087972b949b2b04c453cc3c4bc186
Cesar <cesarnsingi@hotmail.com>
2024-10-23 11:09:31 +0400

    add blogs paragraph

--- Components/AdminComponents/Sidebar.jsx
+++ Components/AdminComponents/Sidebar.jsx
@@ -10,6 +10,7 @@ const Sidebar = () => {
         </div>
         <div className='w-28 sm:w-80 h-[100vh] relative py-12 border border-black'>
             <div className='flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
+                <Image src={assets.add_icon} alt='' width={28} /><p>Add blogs</p>
 
             </div>
 
