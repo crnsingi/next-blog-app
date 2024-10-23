d45f6e5ee1861e29242d5791200e098fa358cd8b
Cesar <cesarnsingi@hotmail.com>
2024-10-23 11:21:11 +0400

    reposition of the add blogs

--- Components/AdminComponents/Sidebar.jsx
+++ Components/AdminComponents/Sidebar.jsx
@@ -9,11 +9,12 @@ const Sidebar = () => {
             <Image src={assets.logo} width={120} alt=''/>
         </div>
         <div className='w-28 sm:w-80 h-[100vh] relative py-12 border border-black'>
+            <div className='w-[50%] sm:w-[80%] absolute right-0'>
             <div className='flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
-                <Image src={assets.add_icon} alt='' width={28} /><p>Add blogs</p>
-
+            <Image src={assets.add_icon} alt='' width={28} /><p>Add blogs</p>
             </div>
-
+           
+            </div>
         </div>
     </div>
   )
