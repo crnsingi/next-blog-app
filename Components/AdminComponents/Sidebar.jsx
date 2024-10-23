f60ab8ba4705ff8bdb8be62dc1033ea22b763d17
Cesar <cesarnsingi@hotmail.com>
2024-10-23 12:16:13 +0400

    fix blog list responsive link

--- Components/AdminComponents/Sidebar.jsx
+++ Components/AdminComponents/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
             <Link href='/admin/addProduct'  className='flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
             <Image src={assets.add_icon} alt='' width={28} /><p>Add blogs</p>
             </Link>
-            <Link href='admin/blogList' className=' mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
+            <Link href='/admin/blogList' className=' mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
             <Image src={assets.blog_icon} alt='' width={28} /><p>Blog lists</p>
             </Link>
             <Link href='/admin/subscriptions' className=' mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
