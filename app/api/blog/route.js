2a48bef62c63e9c62fe7c29a0c8ae40f99dcdb49
Cesar <cesarnsingi@hotmail.com>
2024-10-23 05:39:55 +0400

    add property to POST function

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -16,5 +16,9 @@ export async function GET(request){
 export async function POST(request){
 
     const formData =  await request.formData();
+    const timestamp = Date.now();
 
+    const image = formData.get('image');
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
 }
