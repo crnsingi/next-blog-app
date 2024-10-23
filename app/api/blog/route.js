06b4a46d0517f18edfe8c1452bb05d26c006e5a6
Cesar <cesarnsingi@hotmail.com>
2024-10-23 05:23:02 +0400

    call function ConnectDB

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,4 +1,11 @@
+import { ConnectDB } from "@/lib/config/db"
+
 const { NextResponse } = require("next/server")
+
+const LoadDB = async () => {
+    await ConnectDB();
+
+}
 
 export async function GET(request){
     return NextResponse.json({msg:"API Working"})
