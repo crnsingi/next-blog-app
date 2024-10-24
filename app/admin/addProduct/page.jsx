41dec4b801135c8da1fc62ccd8a11b90835d56d3
Cesar <cesarnsingi@hotmail.com>
2024-10-24 10:12:15 +0400

    basic layout for adding new-blog

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -17,17 +17,17 @@ const page = () => {
         </label>
         <input onChange={(e)=>setImage(e.target.files[0])}  type="file" id='image' hidden required/>
         <p className='text-xl mt-4'>Blog title</p>
-        <input className='w-full sm:w-[500px] mt-4 px-4 py-3 boder'  type="text" placeholder='Type here' required />
+        <input className='w-full sm:w-[500px] mt-4 px-4 py-3 border'  type="text" placeholder='Type here' required />
         <p className='text-xl mt-4'>Blog Description</p>
-        <textarea className='w-full sm:w-[500px] mt-4 px-4 py-3 boder'  type="text" placeholder='write content here' rows={6} required />
+        <textarea className='w-full sm:w-[500px] mt-4 px-4 py-3 border'  type="text" placeholder='write content here' rows={6} required />
         <p className='text-xl mt-4'>Blog category</p>
         <select name="category" className='w-40 mt-4 px-4 py-3 border text-gray-500'>
             <option value="Startup">Startup</option>
             <option value="Technology">Technology</option>
             <option value="Lifestyle">Lifestyle</option>
         </select>
-        <button type="submit"></button>
-
+        <br />
+        <button type="submit" className='mt-8 w-40 h-12 bg-black text-white'>ADD</button>
       </form>
     </>
   )
