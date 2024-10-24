5c522ae270c4fcd0c0fac423f12822ee3620bbed
Cesar <cesarnsingi@hotmail.com>
2024-10-24 11:15:36 +0400

    link property

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -32,9 +32,9 @@ const page = () => {
         </label>
         <input onChange={(e)=>setImage(e.target.files[0])}  type="file" id='image' hidden required/>
         <p className='text-xl mt-4'>Blog title</p>
-        <input name='title' onChange={onChangeHandler} className='w-full sm:w-[500px] mt-4 px-4 py-3 border'  type="text" placeholder='Type here' required />
+        <input name='title' onChange={onChangeHandler} value={data.title} className='w-full sm:w-[500px] mt-4 px-4 py-3 border'  type="text" placeholder='Type here' required />
         <p className='text-xl mt-4'>Blog Description</p>
-        <textarea className='w-full sm:w-[500px] mt-4 px-4 py-3 border'  type="text" placeholder='write content here' rows={6} required />
+        <textarea name='description' onChange={onChangeHandler} value={data.description}  className='w-full sm:w-[500px] mt-4 px-4 py-3 border'  type="text" placeholder='write content here' rows={6} required />
         <p className='text-xl mt-4'>Blog category</p>
         <select name="category" className='w-40 mt-4 px-4 py-3 border text-gray-500'>
             <option value="Startup">Startup</option>
