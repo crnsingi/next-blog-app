329949d21149e948fc6614ec5a7000557ba5eefb
Cesar <cesarnsingi@hotmail.com>
2024-10-24 11:13:07 +0400

    link property

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -17,6 +17,9 @@ const page = () => {
     const onChangeHandler = (event) => {
         const name = event.target.name;
         const value = event.target.value;
+        setData(data=>({...data,[name]:value}))
+        console.log(data);
+        
     }
 
 
@@ -29,7 +32,7 @@ const page = () => {
         </label>
         <input onChange={(e)=>setImage(e.target.files[0])}  type="file" id='image' hidden required/>
         <p className='text-xl mt-4'>Blog title</p>
-        <input className='w-full sm:w-[500px] mt-4 px-4 py-3 border'  type="text" placeholder='Type here' required />
+        <input name='title' onChange={onChangeHandler} className='w-full sm:w-[500px] mt-4 px-4 py-3 border'  type="text" placeholder='Type here' required />
         <p className='text-xl mt-4'>Blog Description</p>
         <textarea className='w-full sm:w-[500px] mt-4 px-4 py-3 border'  type="text" placeholder='write content here' rows={6} required />
         <p className='text-xl mt-4'>Blog category</p>
