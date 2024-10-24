5b2536f2f73984e7c440d9ea45b64a90627b5ee2
Cesar <cesarnsingi@hotmail.com>
2024-10-24 18:36:36 +0400

    add blog function to api

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -12,10 +12,16 @@ const LoadDB = async () => {
 
 LoadDB();
 
+//API Endpoint to get all blogs
+
 export async function GET(request){
-    return NextResponse.json({msg:"API Working"})
+
+    const blogs = await BlogModel.find({});
+    return NextResponse.json({blogs})
 }
 
+
+// API ENDpoint For Uploading Blogs 
 export async function POST(request){
 
     const formData =  await request.formData();
