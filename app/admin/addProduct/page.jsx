b8f45b99a4687d1698c10ad3835ef0d8bf592ed5
Cesar <cesarnsingi@hotmail.com>
2024-10-24 08:07:42 +0400

    add image property

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,4 +1,5 @@
 'use client'
+import { assets } from '@/Assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
@@ -8,7 +9,7 @@ const page = () => {
       <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
         <p className='text-xl'>Upload thumbnail</p>
         <label htmlFor="">
-            <Image src={} />
+            <Image className='mt-4' src={assets.upload_area} width={140} height={70} alt=''/>
         </label>
       </form>
     </>
