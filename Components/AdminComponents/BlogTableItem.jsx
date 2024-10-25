ae40e2dfd43924ccb8b49196475690b9d2cda273
Cesar <cesarnsingi@hotmail.com>
2024-10-25 12:50:32 +0400

    add Image src

--- Components/AdminComponents/BlogTableItem.jsx
+++ Components/AdminComponents/BlogTableItem.jsx
@@ -1,9 +1,16 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
-const BlogTableItem = () => {
+const BlogTableItem = ({authorImg}) => {
   return (
     <tr className='bg-white border-b'>
-
+        <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font font-medium text-gray-900 whitespace-nowrap'>
+            <Image src={authorImg?authorImg:assets.profile_icon} />
+        </th>
+        <td>
+            
+        </td>
     </tr>
   )
 }
