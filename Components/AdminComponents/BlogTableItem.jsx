9982c7d14c0822b01e092066d08274da5b512152
Cesar <cesarnsingi@hotmail.com>
2024-10-25 13:13:17 +0400

    add css property

--- Components/AdminComponents/BlogTableItem.jsx
+++ Components/AdminComponents/BlogTableItem.jsx
@@ -2,14 +2,17 @@ import { assets } from '@/Assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const BlogTableItem = ({authorImg}) => {
+const BlogTableItem = ({authorImg,title}) => {
   return (
     <tr className='bg-white border-b'>
         <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font font-medium text-gray-900 whitespace-nowrap'>
             <Image src={authorImg?authorImg:assets.profile_icon} />
         </th>
-        <td>
-            
+        <td className='px-6 py-4'>
+            {title?title:"no title"}
+        </td>
+        <td className='px-6 py-4'>
+            {"25 Oct 2024"}
         </td>
     </tr>
   )
