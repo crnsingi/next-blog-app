a73b4387a2d54fcc1169680ce92dbdc50f9b832d
Cesar <cesarnsingi@hotmail.com>
2024-10-25 08:02:34 +0400

    add table to bloglist

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -6,9 +6,10 @@ const page = () => {
     <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
       <h1>All blogs</h1>
       <div className='relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'>
+        <table className='w-full'>
 
+        </table>
       </div>
-
     </div>
   )
 }
