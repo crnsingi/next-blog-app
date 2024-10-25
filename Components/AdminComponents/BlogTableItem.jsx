7c6a682128c3f8654eccad034d2664ad4caa6ffa
Cesar <cesarnsingi@hotmail.com>
2024-10-25 12:33:41 +0400

    create Blog Table Item

---
+++ Components/AdminComponents/BlogTableItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogTableItem = () => {
+  return (
+    <tr className='bg-white border-b'>
+
+    </tr>
+  )
+}
+
+export default BlogTableItem
--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -7,13 +7,25 @@ const page = () => {
       <h1>All blogs</h1>
       <div className='relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'>
         <table className='w-full text-sm text-gray-500'>
-          <thead className='text-sm text bg-gray-700 text-left uppercase bg-gray-50'>
+          <thead className='text-sm text text-gray-700 text-left uppercase bg-gray-50'>
             <tr>
               <th scope='col' className='hidden sm:block px-6 py-3'>
                 Author name
               </th>
+              <th scope='col' className='px-6 py-3'>
+                Blog Title
+              </th>
+              <th scope='col' className='px-6 py-3'>
+                Date
+              </th>
+              <th scope='col' className='px-6 py-3'>
+                Action
+              </th>
             </tr>
           </thead>
+          <tbody>
+            
+          </tbody>
         </table>
       </div>
     </div>
