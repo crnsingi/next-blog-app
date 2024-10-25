470ffc7eee77599c9a8d5e129938646abcecbb95
Cesar <cesarnsingi@hotmail.com>
2024-10-25 06:18:11 +0400

    display blog list using API

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
 import BlogItem from './BlogItem'
 import { blog_data } from '@/Assets/assets'
+import axios from 'axios';
 
 const BlogList = () => {
 
     const [menu,setMenu] = useState("All");
+    const [blogs,setBlogs] =useState([]);
+
+    const fetchBlogs = async () =>{
+      const response = await axios.get('/api/blog');
+      setBlogs(response.data.blogs);
+    }
 
   return (
     <div>
