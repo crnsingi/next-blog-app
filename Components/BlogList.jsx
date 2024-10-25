86ab161f9b4ab93dba31006cf886814e5d1c5e8c
Cesar <cesarnsingi@hotmail.com>
2024-10-25 06:38:08 +0400

    add console log to test

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -11,6 +11,7 @@ const BlogList = () => {
     const fetchBlogs = async () =>{
       const response = await axios.get('/api/blog');
       setBlogs(response.data.blogs);
+      console.log(response.data.blogs);
     }
 
     useEffect(()=>{
@@ -26,7 +27,7 @@ const BlogList = () => {
             <button onClick={()=>setMenu('Lifestyle')} className={menu==="Lifestyle"?'bg-black text-white py-1 px-4 rounded-sm' :""}>Lifestyle</button>
         </div>
         <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:max-24 '>
-            {blog_data.filter((item)=> menu==="All"?true:item.category===menu).map((item,index)=>{
+            {blogs.filter((item)=> menu==="All"?true:item.category===menu).map((item,index)=>{
                 return <BlogItem key={index} id={item.id} image={item.image} title={item.title} description={item.description} category={item.category}/>
             })}
         </div>
