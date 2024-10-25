215d3cee457a083a9102753f782d758a583b52bb
Cesar <cesarnsingi@hotmail.com>
2024-10-25 06:35:00 +0400

    add useEffect

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BlogItem from './BlogItem'
 import { blog_data } from '@/Assets/assets'
 import axios from 'axios';
@@ -12,6 +12,10 @@ const BlogList = () => {
       const response = await axios.get('/api/blog');
       setBlogs(response.data.blogs);
     }
+
+    useEffect(()=>{
+      fetchBlogs();
+    },[])
 
   return (
     <div>
