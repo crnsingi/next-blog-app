b69872662f64214790e2692e06ada1efeb5bcda5
Cesar <cesarnsingi@hotmail.com>
2024-10-26 16:32:25 +0400

    add Blog List

--- app/admin/subscriptions/page.jsx
+++ app/admin/subscriptions/page.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+      <div>
+        
+      </div>
+      <h1>All Subscription</h1>
     </div>
   )
 }
