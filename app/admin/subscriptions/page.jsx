7e9b45ee0e2b4a115e430db620d1ea64b927617d
Cesar <cesarnsingi@hotmail.com>
2024-10-26 16:49:21 +0400

    add a thead

--- app/admin/subscriptions/page.jsx
+++ app/admin/subscriptions/page.jsx
@@ -7,7 +7,9 @@ const page = () => {
             <h1>All Subscription</h1>
       <div className='relative max-w-[600px] h-[80vh] overflow-x-auto mt-4 border border-gray-400 scollbar-hide'>
         <table className='w-full text-sm text-gray-500'>
+          <thead className='text-xs text-left text-gray-700 uppercase bg-gray-50'>
 
+          </thead>
         </table>
       </div>
     </div>
