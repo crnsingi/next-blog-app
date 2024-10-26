d4bf909737b496ad4138f4e98b219303d5bc8521
Cesar <cesarnsingi@hotmail.com>
2024-10-26 16:41:59 +0400

    add div to subs page

--- app/admin/subscriptions/page.jsx
+++ app/admin/subscriptions/page.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
-      <div>
-        
+            <h1>All Subscription</h1>
+      <div className='relative max-w-[600px] h-[80vh] overflow-x-auto mt-4 border border-gray-400'>
+
       </div>
-      <h1>All Subscription</h1>
     </div>
   )
 }
